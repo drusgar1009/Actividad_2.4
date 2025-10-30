625bddd401668e7c79bac11820fd212ce07a0d40
Diego Jesus <drusgar1009@g.educaand.es>
2025-10-30 12:52:48 +0100

    Adios mundo

--- Hola_Mundo.java
+++ Hola_Mundo.java
@@ -1,5 +1,7 @@
 public class Hola_Mundo {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        System.out.println();
+        System.out.println("Adiós mundo");
     }
 }
